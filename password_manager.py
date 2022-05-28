677f2a53aa3c09c08b92a55d0613f0bf20022611
Tepe13 <tepelenas@gmail.com>
2022-05-28 10:12:50 +0100

    adding everything in a loop

--- password_manager.py
+++ password_manager.py
@@ -1,3 +1,20 @@
 master_pwd=input("What is the master password? ")
 
-mode=input("Would you like to add a new password or view existing ones (view,add)")
+def view():
+    pass
+
+def add():
+    pass
+
+
+while True:
+    mode=input("Would you like to add a new password or view existing ones (view,add), press q to quit ").lower()
+    if mode =="q":
+        break
+    
+    if mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    else:
+        print("Invalid mode.")
