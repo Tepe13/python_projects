033b4a7cc374113f4b03523aab471294cb4379d7
Tepe13 <tepelenas@gmail.com>
2022-05-28 12:01:16 +0100

    added encryption to passwords

--- password_manager.py
+++ password_manager.py
@@ -1,11 +1,23 @@
 from cryptography.fernet import Fernet
 
-master_pwd=input("What is the master password? ")
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
 
+
+master_pwd=input("What is the master password? ")
+key = load_key() + master_pwd.encode
+fer = Fernet(key)
+
+'''
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)
+        key_file.write(key)'''
+
+
 
 def view():
     with open('passwords.txt', 'r') as f:
@@ -20,7 +32,7 @@ def add():
     pwd = input ("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name +"|" + pwd + "\n")
+        f.write(name +"|" + str(fer.encrypt(pwd.encode())) + "\n")
 
 
 while True:
