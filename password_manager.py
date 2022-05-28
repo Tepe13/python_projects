54846e799409fe2d028abf13e5dd5f9ba663a756
Tepe13 <tepelenas@gmail.com>
2022-05-28 10:23:05 +0100

    editing view function

--- password_manager.py
+++ password_manager.py
@@ -1,13 +1,16 @@
 master_pwd=input("What is the master password? ")
 
 def view():
-    pass
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            print(line)
 
 def add():
     name = input ('Account Name: ')
     pwd = input ("Password: ")
 
     with open('passwords.txt', 'a') as f:
+        f.write(name +"|" + pwd +"\n")
 
 
 while True:
