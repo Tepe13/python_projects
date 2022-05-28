693c0b0727534ed78c0fb54635661b851568a43a
Tepe13 <tepelenas@gmail.com>
2022-05-28 11:19:36 +0100

    fixed the view issue

--- password_manager.py
+++ password_manager.py
@@ -1,18 +1,26 @@
+from cryptography.fernet import Fernet
+
 master_pwd=input("What is the master password? ")
 
+'''def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
-            user, passw = data.split("|")
-            print("User:", user, "| Password:", passw)
+            if data and "|" in data:
+                user, passw = data.split("|")
+                print("User:", user, "| Password:", passw)
 
 def add():
     name = input ('Account Name: ')
     pwd = input ("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name +"|" + pwd +"\n")
+        f.write(name +"|" + pwd + "\n")
 
 
 while True:
